001cb158966c34582cd6fadc70df4e234c2f26ee
nickmanks <nick.manks@gmail.com>
2019-02-02 12:47:15 +1100

    feat(project): project initialisation

---
+++ src/write.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import {join} from 'path';
+
+
+const JSON_SPACING = 4;
+
+
+export const main = (process)=> {
+  const remarkConfig = {
+    plugins: ['remark-preset-lint-recommended']
+  };
+
+  fs.writeFileSync(
+    join(process.cwd(), '.remarkrc'),
+    JSON.stringify(remarkConfig, null, JSON_SPACING)
+  );
+};
+
+
+/* istanbul ignore if */
+if (require.main === module) {
+  main(process);
+}
---
+++ src/write.test.js
@@ -0,0 +1,32 @@
+import {main} from './write';
+import {join} from 'path';
+import {writeFileSync} from 'fs';
+
+
+jest.mock('path', ()=> ({
+  join: jest.fn(()=> 'test-path')
+}));
+
+jest.mock('fs', ()=> ({
+  writeFileSync: jest.fn((path, json)=> `${path}-${json}`)
+}));
+
+const path = process.cwd();
+
+
+describe('Writing .remarkrc file', ()=> {
+  it('writes the file to the path', ()=> {
+    const remarkConfig = {
+      plugins: ['remark-preset-lint-recommended']
+    };
+    const JSON_SPACING = 4;
+
+    main(process);
+
+    expect(join).toHaveBeenCalledWith(path, '.remarkrc');
+    expect(writeFileSync).toHaveBeenCalledWith(
+      'test-path',
+      JSON.stringify(remarkConfig, null, JSON_SPACING)
+    );
+  });
+});
